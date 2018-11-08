60a4ee1f20652c8c70fc9b66c8490883b68d4f50
Dinar1975 <kdinar@mail.ru>
2018-11-08 20:05:25 +0600

    Выполнена задача №7 Лаб.работа №7

--- Лабораторная_07/Задача_07/Program.cs
+++ Лабораторная_07/Задача_07/Program.cs
@@ -17,210 +17,22 @@ namespace Задача_07
 
             String t = arr[0];
 
-            arr[3] = arr[2];
+            t = arr[0];                                     // Гипотеза 7
             int i = 0;
-            while (i < arr.Length)
-            {
-                Console.WriteLine(" " + arr[i] + i);
-                i = i + 1;
-            }
-            Console.WriteLine();
-
-            arr[4] = arr[3];
-            i = 0;
-            while (i < arr.Length)
-            {
-                Console.WriteLine(" " + arr[i] + i);
-                i = i + 1;
-            }
-            Console.WriteLine();
-
+            arr[0] = arr[5];
             arr[5] = arr[4];
-            i = 0;
-            while (i < arr.Length)
-            {
-                Console.WriteLine(" " + arr[i] + i);
-                i = i + 1;
-            }
-            Console.WriteLine();
-
-            t = arr[5];
-            i = 0;
-            while (i < arr.Length)
-            {
-                Console.WriteLine(" " + arr[i] + i);
-                i = i + 1;
-            }
-            Console.WriteLine();
-
-            arr[0] = t;
-            i = 0;
-            while (i < arr.Length)
-            {
-                Console.WriteLine(" " + arr[i] + i);
-                i = i + 1;
-            }
-            Console.WriteLine();
-
-            arr[1] = arr[0];
-            i = 0;
-            while (i < arr.Length)
-            {
-                Console.WriteLine(" " + arr[i] + i);
-                i = i + 1;
-            }
-            Console.WriteLine();
-
+            arr[4] = arr[3];
+            arr[3] = arr[2];
             arr[2] = arr[1];
-            i = 0;
+            arr[1] = t;
+
             while (i < arr.Length)
             {
                 Console.WriteLine(" " + arr[i] + i);
                 i = i + 1;
             }
             Console.WriteLine();
-
                         
-            
-            
-            //arr[1] = arr[0];           // Гипотеза 2  
-            //i = 0;
-            //while (i < arr.Length)
-            //{
-            //    Console.Write(" " + arr[i] + " " + i);
-            //    i = i + 1;
-            //}
-            //Console.WriteLine();
-
-            //arr[2] = arr[1];           // Получаем q0 q1 q2 q3 q4 q5  
-            //i = 0;
-            //while (i < arr.Length)
-            //{
-            //    Console.Write(" " + arr[i] + " " + i);
-            //    i = i + 1;
-            //}
-            //Console.WriteLine();
-
-            //arr[3] = arr[2];
-            //i = 0;
-            //while (i < arr.Length)
-            //{
-            //    Console.Write(" " + arr[i] + " " + i);
-            //    i = i + 1;
-            //}
-            //Console.WriteLine();
-
-            //arr[4] = arr[3];
-            //i = 0;
-            //while (i < arr.Length)
-            //{
-            //    Console.Write(" " + arr[i] + " " + i);
-            //    i = i + 1;
-            //}
-            //Console.WriteLine();
-
-            //arr[5] = arr[4];
-            //i = 0;
-            //while (i < arr.Length)
-            //{
-            //    Console.Write(" " + arr[i] + " " + i);
-            //    i = i + 1;
-            //}
-            //Console.WriteLine();
-
-            //t = arr[5];
-            //i = 0;
-            //while (i < arr.Length)
-            //{
-            //    Console.Write(" " + arr[i] + " " + i);
-            //    i = i + 1;
-            //}
-            //Console.WriteLine();
-
-            //arr[0] = t;
-            //i = 0;
-            //while (i < arr.Length)
-            //{
-            //    Console.Write(" " + arr[i] + " " + i);
-            //    i = i + 1;
-            //}
-            //Console.WriteLine();
-
-
-
-            //t = arr[5];                // Гипотеза 1 
-
-            //int i = 0;
-            //while (i < arr.Length)
-            //{
-            //    Console.Write(" " + arr[i] + " " + i);
-            //    i = i + 1;
-            //}
-            //Console.WriteLine();
-
-            //arr[0] = t;                // Получаем y0 y1 y2 y3 y4 y5  
-            //i = 0;
-            //while (i < arr.Length)
-            //{
-            //    Console.Write(" " + arr[i] + " " + i);
-            //    i = i + 1;
-            //}
-            //Console.WriteLine();
-
-            //arr[1] = arr[0];
-            //i = 0;
-            //while (i < arr.Length)
-            //{
-            //    Console.Write(" " + arr[i] + " " + i);
-            //    i = i + 1;
-            //}
-            //Console.WriteLine();
-
-            //arr[2] = arr[1];
-            //i = 0;
-            //while (i < arr.Length)
-            //{
-            //    Console.Write(" " + arr[i] + " " + i);
-            //    i = i + 1;
-            //}
-            //Console.WriteLine();
-
-            //arr[3] = arr[2];
-            //i = 0;
-            //while (i < arr.Length)
-            //{
-            //    Console.Write(" " + arr[i] + " " + i);
-            //    i = i + 1;
-            //}
-            //Console.WriteLine();
-
-            //arr[4] = arr[3];
-            //i = 0;
-            //while (i < arr.Length)
-            //{
-            //    Console.Write(" " + arr[i] + " " + i);
-            //    i = i + 1;
-            //}
-            //Console.WriteLine();
-
-            //arr[5] = arr[4];
-            //i = 0;
-            //while (i < arr.Length)
-            //{
-            //    Console.Write(" " + arr[i] + " " + i);
-            //    i = i + 1;
-            //}
-            //Console.WriteLine();
-
-            //t = arr[5];
-
-            i = 0;
-            while (i < arr.Length)
-            {
-                Console.Write(" " + arr[i] + " " + i);
-                i = i + 1;
-            }
-            Console.WriteLine();
 
         }
     }
