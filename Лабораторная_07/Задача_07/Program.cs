dfb3866a307834a48c1ecca4823c364248e45e7d
Dinar1975 <kdinar@mail.ru>
2018-11-15 20:47:42 +0600

    Решена задача №7 Лаб. работа №7

--- Лабораторная_07/Задача_07/Program.cs
+++ Лабораторная_07/Задача_07/Program.cs
@@ -17,26 +17,89 @@ namespace Задача_07
 
             String t = arr[0];
 
-            t = arr[0];                                     // Гипотеза 7
-            int i = 0;
-            int n = 1;
-            n = n - 1;
-            arr[n] = arr[5];    // n=0
-            n = n + 5;
-            arr[n] = arr[4];    // n=5
-            n = n - 1;
-            arr[n] = arr[3];    // n=4
-            n = n - 1;
-            arr[n] = arr[2];    // n=3
-            n = n - 1;
-            arr[n] = arr[1];    // n=2
-            n = n - 1;
-            arr[n] = t;         // n=1
+            //                                                  // Гипотеза 8
+            //Console.WriteLine(0 + "," + 5);
+            //Console.WriteLine(5 + "," + 4);
+            //Console.WriteLine(4 + "," + 3);
+            //Console.WriteLine(3 + "," + 2);
+            //Console.WriteLine(2 + "," + 1);
+            //Console.WriteLine(1);
+
+            //int l = 0;
+            //int r = 5;
+            //Console.WriteLine(l + "," + r);
+            //l = l + 5;
+            //r = r - 1;
+            //Console.WriteLine(l + "," + r);
+            //l = l - 1;
+            //r = r - 1;
+            //Console.WriteLine(l + "," + r);
+            //l = l - 1;
+            //r = r - 1;
+            //Console.WriteLine(l + "," + r);
+            //l = l - 1;
+            //r = r - 1;
+            //Console.WriteLine(l + "," + r);
+            //Console.WriteLine(1);
 
 
 
+            //int l = 0;
+            //int r = 5;
+            //Console.WriteLine(l + "," + r);
+            //l = l + 5;
+            //r = r - 1;
+            ////Console.WriteLine(l + "," + r);
+            //while (l > 1)
+            //{
+            //    Console.WriteLine(l + "," + r);
+            //    l = l - 1;
+            //    r = r - 1;
+            //}
+            //Console.WriteLine(1);
 
+            //t = arr[0];                                     // Гипотеза 7
+            //int l = 0;
+            //int r = 5;                        
+            //arr[0] = arr[r];    // n=0
 
+            //l = l + 5;
+            //r = r - 1;
+            //arr[l] = arr[r];    // n=5
+
+            //l = l - 1;
+            //r = r - 1;
+            //arr[l] = arr[r];    // n=4
+
+            //l = l - 1;
+            //r = r - 1;
+            //arr[l] = arr[r];    // n=3
+
+            //l = l - 1;
+            //r = r - 1;
+            //arr[l] = arr[r];    // n=2
+
+            //arr[1] = t;         // n=1
+
+            
+            int l = 0;
+            int r = arr.Length;
+            r = r - 1;
+            arr[l] = arr[r];
+            Console.WriteLine(l + "," + r);
+
+            arr[l] = arr[r];
+            l = l + arr.Length-1;
+            r = r - 1;            
+            while(l > 1)
+            {
+                arr[l] = arr[r];
+                Console.WriteLine(l + "," + r);
+                l = l - 1;
+                r = r - 1;
+            }
+            arr[l] = t;
+            Console.WriteLine(1);
             //arr[n] = arr[n+5];
             //arr[n+5] = arr[n+4];
             //arr[n+4] = arr[n+3];
@@ -44,13 +107,15 @@ namespace Задача_07
             //arr[n+2] = arr[n+1];
             //arr[n+1] = t;
 
+            int i = 0;
+
             while (i < arr.Length)
             {
                 Console.WriteLine(" " + arr[i] + i);
                 i = i + 1;
             }
             Console.WriteLine();
-                        
+
 
         }
     }
